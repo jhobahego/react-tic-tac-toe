33fc9d60b56bca4b70511f82fe8af5cb28d9a09b
Jhon Hernandez <jhobahego16@hotmail.com>
2023-04-06 18:25:53 -0500

    tablero creado

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
+import { useState } from 'react'
+
 function App() {
+  const [board, setBoard] = useState(
+    Array(9).fill(null)
+  )
 
   return (
     <main className="board">
       <h1>Tic tac toe</h1>
+      <section className="game">
+        {
+          board.map((_, index) => {
+            return (
+              <div className='square'>
+                {index}
+              </div>
+            )
+          })
+        }
+      </section>
     </main>
   )
 }
