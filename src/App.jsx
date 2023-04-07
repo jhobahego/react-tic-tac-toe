cd30810b84a5055de0bdf7d0927decfbf85cfa88
Jhon Hernandez <jhobahego16@hotmail.com>
2023-04-07 15:41:58 -0500

    Se lanza confetti al ganar un juego

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import confetti from 'canvas-confetti'
 
 const TURNS = {
   X: 'x',
@@ -68,6 +69,7 @@ function App() {
 
     const newWinner = checkWinner(newBoard)
     if(newWinner){
+      confetti()
       setWinner(newWinner)
     }else if(checkEndGame(newBoard)){
       setWinner(false)
