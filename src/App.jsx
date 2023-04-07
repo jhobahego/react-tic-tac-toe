95b52560c6f9efd145b4611563ac0b63a3c62418
Jhon Hernandez <jhobahego16@hotmail.com>
2023-04-07 13:40:49 -0500

    estado y funcionalidad de verificar ganador

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,17 @@ const TURNS = {
   O: 'o'
 }
 
+const WINNING_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
 const Square = ({ children, isSelected, updateBoard, index }) => {
   const className = `square ${isSelected ? 'is-selected' : ''}`
 
@@ -23,15 +34,38 @@ function App() {
   const [board, setBoard] = useState(
     Array(9).fill(null)
   )
-  const [turn, setTurn] = useState(TURNS.x)
+  const [turn, setTurn] = useState(TURNS.X)
+  const [winner, setWinner] = useState(null)
+
+  const checkWinner = (boardToCheck) => {
+    for(const combo of WINNING_COMBINATIONS){
+      const [a, b, c] = combo
+      if(
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+      ){
+        return boardToCheck[a]
+      }
+    }
+
+    return null
+  }
 
   const updateBoard = (index) => {
+    if (board[index] || winner) return;
+
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
 
     const newBoard = [...board]
     newBoard[index] = turn
     setBoard(newBoard)
+
+    const newWinner = checkWinner(newBoard)
+    if(newWinner){
+      setWinner(newWinner)
+    }
   }
 
   return (
